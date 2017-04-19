4203bcea001afd68d8d65103be129023ba9854a8
Nick Powell and Sowmya Dinavahi <nick+sowmya@>
2017-04-19 12:04:59 -0700

    add result instead of title

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ get('/') do
   erb(:index)
 end
 
-get('/title') do
-  @title = params.fetch('title').scrabble()
-  erb(:title)
+get('/result') do
+  @result = params.fetch('result').scrabble()
+  erb(:result)
 end
--- spec/scrabble_integration_spec.rb
+++ spec/scrabble_integration_spec.rb
@@ -6,7 +6,7 @@ set(:show_exceptions, false)
 describe('the word play path', {:type => :feature}) do
   it('processes the user entry and returns it with number') do
     visit('/')
-    fill_in('title', :with => 'epicodus')
+    fill_in('result', :with => 'epicodus')
     click_button('Send')
     expect(page).to have_content(13)
   end
